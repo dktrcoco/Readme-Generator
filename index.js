c66cc36263ceaf6af2e91ff7ce6b2c4ac3cc5f4e
Chris Kabana <chris.kabana@gmail.com>
2020-08-29 21:42:57 -0500

    trying to add things while using the gitignore

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ function promptUser() {
       message: "Please provide detailed information regarding usage of your project."
     },
     {
+      type: "list",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0"], 
+      name: "license",
+      message: "Please provide any relevant information regarding licensing for this project."
+    },
+    {
       type: "input",
       name: "contribution",
       message: "Please provide information on how to contribute to your project."
@@ -35,43 +41,77 @@ function promptUser() {
       type: "input",
       name: "testing",
       message: "Please provide instructions on how to properly test your project."
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please provide your GitHub Username."
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please input your email address."
     }
   ]);
 }
 
-function generateHTML(answers) {
+function generateMD(answers) {
   return `
-  ${answers.title}
-  #${answers.title}
-  $#{answers.title}
-  #Title
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+ # Project Title: ${answers.title}
+
+ ## Description
+
+ ${answers.description}
+
+ ## Table of Contents
+
+  * [Install](#install)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+
+ ## Install
+
+ ${answers.install}
+
+ ## Usage
+
+ ${answers.usage}
+
+ ## License
+ 
+ ![github license](https://img.shields.io/badge/label-${answers.license}-green)
+
+ ## Contribution
+ 
+ ${answers.contribution}
+
+ ## Testing
+ 
+ ${answers.testing}
+
+ ## Questions
+ 
+ If you have any questions that are not answered here, please feel free to reach out to me at my email (${answers.email}).
+ You can also reach out to me on github by searching for my username: ${answers.github} (Link: https://github.com/${answers.github}).
+
+      `;
 }
 
 async function init() {
-  console.log("hi")
+
   try {
     const answers = await promptUser();
 
-    const readme = generateHTML(answers);
+    const readme = generateMD(answers);
 
     await writeFileAsync("test.md", readme);
 
     console.log("Successfully wrote to test.md");
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
---
+++ test.md
@@ -0,0 +1,43 @@
+
+ # Project Title: aSD
+
+ ## Description
+
+ ASDF
+
+ ## Table of Contents
+
+  * [Install](#install)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Testing](#testing)
+  * [Questions](#questions)
+
+
+ ## Install
+
+ 
+
+ ## Usage
+
+ asdf
+
+ ## License
+ 
+ ![github license](https://img.shields.io/badge/label-MIT-green)
+
+ ## Contribution
+ 
+ asdf
+
+ ## Testing
+ 
+ asdf
+
+ ## Questions
+ 
+ If you have any questions that are not answered here, please feel free to reach out to me at my email (asdf).
+ You can also reach out to me on github by searching for my username: asdf (Link: https://github.com/asdf).
+
+      
